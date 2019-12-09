08e44eb5381b3242c7f8647afbb8e585f4318174
Nguyễn Vương Khang Hy <khanghy3004@gmail.com>
2019-12-09 09:12:24 +0700

    final

--- src/src/main/ConnectionDB.java
+++ src/src/main/ConnectionDB.java
@@ -1,6 +1,10 @@
 package main;
+
 import java.sql.*;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  *
@@ -8,72 +12,245 @@ import java.util.Scanner;
  */
 public class ConnectionDB {
     Connection cnn = null;
-    public String customerID, username, password, fullname, mail, phoneNumber, address;
+    /*ACCOUNT*/
+    public String username, password, fullname, mail, phoneNumber, address, admin;
+    /*BILL*/
+    public String billID, productID, productName,dateBuy;
+    public int warranty;
+    /*REPORT*/
+    public String reportID, reportContent, dateReport;
+    int status;
+
+    ArrayList<ConnectionDB> listBillDB = new ArrayList<>();
+    ArrayList<ConnectionDB> listReportDB = new ArrayList<>();
+    SortedSet<String> sortedSetUsername = new TreeSet<>(); 
+    
     String sql;
     public ConnectionDB() {
-        this.customerID = "";
         this.username = "";
         this.password = "";
         this.fullname = "";
         this.mail = "";
         this.phoneNumber = "";
-        this.address = "";
+        this.address = "No info";
+    }
+    // Constructor contain Bill details
+    public ConnectionDB(String billID, String username, String productID, String productName, int warranty, String dateBuy) {
+        this.username = username;
+        this.billID = billID;
+        this.productID = productID;
+        this.productName = productName;
+        this.warranty = warranty;
+        this.dateBuy = dateBuy; 
+    }
+    // Constructor contain Report details
+    public ConnectionDB(String reportID, String username, String billID, String productName, String reportContent, String dateReport, int status) {
+        this.reportID = reportID;
+        this.username = username;
+        this.billID = billID;
+        this.productName = productName;
+        this.reportContent = reportContent;
+        this.dateReport = dateReport;
+        this.status = status;
     }
     
-    public ConnectionDB(String customerID, String username, String password, String fullname, String mail, String phoneNumber, String address) {
-        this.customerID = customerID;
-        this.username = username;
-        this.password = password;
-        this.fullname = fullname;
-        this.mail = mail;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-    }
-    public String getCusID() {
-        sql =  "Select MAX(CustomerID) from Customer";
+    public String getBilltID(Connection cn) {
+        sql =  "Select MAX(BillID) from Bill";
         try {
-            Statement stm = cnn.createStatement();
+            Statement stm = cn.createStatement();
             ResultSet rs = stm.executeQuery(sql);   
             if(rs.next()) {
-                String number = rs.getString(1).substring(1);
-                int result = Integer.parseInt(number);
-                result++;
-                String ID = rs.getString(1).charAt(0)+String.valueOf(result);
-                return ID;
+                return rs.getString(1).charAt(0)+String.valueOf(Integer.parseInt(rs.getString(1).substring(1))+1);
             }          
         } catch (SQLException e) {
-            System.out.println("Customer ID error");
+            System.out.println("Bill ID error. "+e.getMessage());
         }
         return "";
     }
-    public boolean checkRegister(Connection cn, String username, String password, String fullname, String mail, String phoneNumber) {
-        sql = String.format("Insert into Customer(CustomerID, Username, Password, Fullname, Mail, PhoneNumber, Address)  values ('%s', '%s', '%s', '%s', '%s', '%s', '%s')", getCusID(), username, password, fullname, mail, phoneNumber, address);
-        System.out.println(sql);
+    
+    public boolean checkInsertBill(Connection cn, String username, String productID, String productName, String warranty) {
+        sql = String.format("Insert into Bill values ('%s', '%s', '%s', '%s', '%s', '%s')", this.getBilltID(cn), username, productID, productName, warranty, LocalDate.now().toString());
+        //System.out.println(sql);
+        try {
+            Statement stm = cnn.createStatement();
+            stm.executeUpdate(sql);  
+            System.out.println("Insert success!");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Bill error 1: "+e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("Bill error 2: "+e.getMessage());    
+        } catch (Exception e) {
+            System.out.println("Bill error 3: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean checkDisplayAllReport(Connection cn) {
+        sql = "Select * from Report";
         try {
             Statement stm = cn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            this.listReportDB.clear();
+            while (rs.next()) {
+                this.listReportDB.add(new ConnectionDB(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), Integer.parseInt(rs.getString(7))));
+            }
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Report error 1: "+e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Report error 2: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Report error 3: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean checkUsernameOnBill(Connection cn) {
+        sql = "Select username from Bill";
+        try {
+            Statement stm = cn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            this.sortedSetUsername.clear();
+            while (rs.next()) {
+                this.sortedSetUsername.add(rs.getString(1));
+            }
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error 1: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error 2: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public String getReportID(Connection cn) {
+        sql =  "Select MAX(ReportID) from Report";
+        try {
+            Statement stm = cn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);   
+            if(rs.next()) {
+                return rs.getString(1).charAt(0)+String.valueOf(Integer.parseInt(rs.getString(1).substring(1))+1);
+            }          
+        } catch (SQLException e) {
+            System.out.println("Report ID error. "+e.getMessage());
+        }
+        return "";
+    }
+
+    public void changStatusReport(Connection cn, String reportID) {
+        sql = "Update Report SET status='1' WHERE ReportID = '"+reportID+"'";
+        try {
+            Statement stm = cnn.createStatement();
+            stm.executeUpdate(sql);  
+            System.out.println("Update success!");
+        } catch (SQLException e) {
+            System.out.println("Report error 1: "+e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("Report error 2: "+e.getMessage());    
+        } catch (Exception e) {
+            System.out.println("Report error 3: "+e.getMessage());
+        }
+
+    }
+    public boolean checkInsertReport(Connection cn, String username, String billID, String productName, String reportContent) {
+        sql = String.format("Insert into Report values ('%s', '%s', '%s', '%s', '%s', '%s', '0')", this.getReportID(cn), username, billID, productName, reportContent, LocalDate.now().toString());
+        //System.out.println(sql);
+        try {
+            Statement stm = cnn.createStatement();
+            stm.executeUpdate(sql);  
+            System.out.println("Report success!");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Report error 1: "+e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("Report error 2: "+e.getMessage());    
+        } catch (Exception e) {
+            System.out.println("Report error 3: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean checDisplayReport(Connection cn, String username) {
+        sql = "Select * from Report where username='"+username+"'";
+        try {
+            Statement stm = cn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            this.listReportDB.clear();
+            while (rs.next() && rs.getString(2).equals(username)) {
+                this.listReportDB.add(new ConnectionDB(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), Integer.parseInt(rs.getString(7))));
+            }
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Report error 1: "+e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Report error 2: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Report error 3: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean checkBill(Connection cn, String username) {
+        sql = "Select * from Bill where username='"+username+"'";
+        try {
+            Statement stm = cn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            this.listBillDB.clear();
+            while (rs.next() && rs.getString(2).equals(username)) {
+                this.listBillDB.add(new ConnectionDB(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), Integer.parseInt(rs.getString(5)), rs.getString(6)));
+            }
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Bill error 1: "+e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Bill error 2: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Bill error 3: "+e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean checkRegister(Connection cn, String username, String password, String fullname, String mail, String phoneNumber) {
+        sql = String.format("Insert into Account(Username, Password, Fullname, Mail, PhoneNumber)  values ('%s', '%s', '%s', '%s', '%s')", username, password, fullname, mail, phoneNumber);
+        //System.out.println(sql);
+        try {
+            Statement stm = cnn.createStatement();
             stm.executeUpdate(sql);  
             System.out.println("Register success");
             return true;
         } catch (SQLException e) {
-            System.out.println("Register Fail");
+            System.out.println("Register Fail. Username has exist");
+        } catch (NullPointerException e) {
+            System.out.println("Register Fail. NULL");
+        } catch (Exception e) {
+            System.out.println("Register error: "+e.getMessage());
         }
         return false;
     }
+    
     public boolean checkLogin(Connection cn, String username, String password) {
-        sql = "Select * from Customer where username='"+username+"'";
-        System.out.println(sql);
+        sql = "Select * from Account where username='"+username+"'";
+        //System.out.println(sql);
         try {
             Statement stm = cn.createStatement();
-            ResultSet rs = stm.executeQuery(sql);   
-            if(rs.next() && rs.getString(2).equals(username) && rs.getString(3).equals(password)) {
-                System.out.println("Login success");
+            ResultSet rs = stm.executeQuery(sql);
+            // Check username and password is correct
+            
+            if(rs.next() && rs.getString(1).equals(username) && rs.getString(2).equals(password)) {
+                this.fullname = rs.getString(3);
+                this.mail = rs.getString(4);
+                this.phoneNumber = rs.getString(5);
+                this.address = rs.getString(6);
+                this.admin = rs.getString(7);
                 return true;
             }
-            
         } catch (SQLException e) {
-            System.out.println("Login Fail");
+            System.out.println("Login Fail. "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Login error: "+e.getMessage());
         }
-        return false;
+        return false; // if not correct
     }
     
     public Connection getConnectDB() {
@@ -84,8 +261,8 @@ public class ConnectionDB {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             cnn = DriverManager.getConnection(uRL, user, pass);
             System.out.println("Success!");
-        } catch (Exception e) {
-            System.out.println("Cannot connect");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Cannot connect. "+e.getMessage());
         }
         return cnn;
     }
--- src/src/main/Main.java
+++ src/src/main/Main.java
@@ -6,6 +6,8 @@
 package main;
 
 import java.sql.*;
+import java.text.ParseException;
+import java.util.Iterator;
 import java.util.Scanner;
 /**
  *
@@ -16,36 +18,147 @@ public class Main {
     static Connection cn = connect.getConnectDB();
     static Scanner input = new Scanner(System.in);
     
-    static void login() {
-        Person person = new Person();
+    static Person currentPerson = new Person();
+    
+    
+    static void customer(Customer KH) throws ParseException {
+        boolean check = true;
         do {
-            person.login();
-            if(connect.checkLogin(cn, person.getUsername(), person.getPassword())) {
-                System.out.println("New page");
+            System.out.println("Hello "+KH.getFullname());
+            System.out.println("1. My profile");
+            System.out.println("2. My Bill");
+            System.out.println("3. Make a report");
+            System.out.println("4. Display my report");
+            System.out.println("5. Log out");
+            String choice = input.nextLine();
+            switch(choice) {
+                case "1": KH.displayProfile(); break;
+                case "2": KH.displayBill(KH.getUsername()); break;
+                case "3": KH.makeReport(KH.getUsername()); break;
+                case "4": KH.displayReport(KH.getUsername());break;
+                case "5": check = false; break;
+                default: System.out.println("Please enter again (1-5)");
             }
-            else {
-                System.out.println("Press \"ENTER\" to continue, another key to exit");
-                String check = input.nextLine();
-                if (!check.equals("")) break;
-                
-            }
-        } while(true);
+        } while(check);   
     }
-    static void register() {
-        Person person = new Person();
+    static void admin(Customer KH) {
+        System.out.println("Hello admin "+KH.getFullname());
+        System.out.println("1. My profile");
+        System.out.println("2. Logout");
+        String choice = input.nextLine();
+        switch(choice) {
+            case "1": KH.displayProfile(); break;
+            case "2": break;
+        }
+    }
+    static void frontend(Admin frontend) throws ParseException {
+        boolean check = true;
         do {
+            System.out.println("Hello "+frontend.getFullname());
+            System.out.println("1. My profile");
+            System.out.println("2. Display Customer Bill and Report");
+            System.out.println("3. Make a report");
+            System.out.println("4. Log out");
+            String choice = input.nextLine();
+            switch(choice) {
+                case "1": 
+                    frontend.displayProfile(); 
+                    break;
+                case "2": 
+                    frontend.customerOnBillandReport();   
+                    break;
+                case "3":
+                    System.out.println("Enter Customer Username: ");
+                    String user = input.nextLine();
+                    frontend.makeReport(user); break;
+                case "4": check = false; break;
+                default: System.out.println("Please enter again (1-4)");
+            }
+        } while(check);  
+    }
+    
+    static void backend(Admin backend) throws ParseException {
+        boolean check = true;
+        do {
+            System.out.println("Hello "+backend.getFullname());
+            System.out.println("1. My profile");
+            System.out.println("2. Display Customer Bill and Report");
+            System.out.println("3. Report Handling");
+            System.out.println("4. Log out");
+            String choice = input.nextLine();
+            switch(choice) {
+                case "1": 
+                    backend.displayProfile(); 
+                    break;
+                case "2": 
+                    backend.customerOnBillandReport();   
+                    break;
+                case "3":
+                    backend.reportHandling(); break;
+                case "4": check = false; break;
+                default: System.out.println("Please enter again (1-4)");
+            }
+        } while(check);  
+    }
+    
+    static void order(Admin order) {
+        boolean check = true;
+        do {
+            System.out.println("Hello "+order.getFullname());
+            System.out.println("1. My profile");
+            System.out.println("2. Insert a new customer bill");
+            System.out.println("3. Log out");
+            String choice = input.nextLine();
+            switch(choice) {
+                case "1": 
+                    order.displayProfile(); 
+                    break;
+                case "2": 
+                    order.insertNewBill();   
+                    break;
+                case "3": check = false; break;
+                default: System.out.println("Please enter again (1-3)");
+            }
+        } while(check);  
+    }
+    
+    static void mainLogin() {
+        try {
+            currentPerson.login();
+            
+            if (currentPerson.getAdmin().equals("1")) { 
+                //admin(KH);
+            } 
+            else if (currentPerson.getAdmin().equals("2")) {
+                Admin frontend = new Admin(currentPerson.getUsername(), currentPerson.getFullname(), currentPerson.getMail(), currentPerson.getPhoneNumber(), currentPerson.getAddress());
+                frontend(frontend);
+            }
+            else if (currentPerson.getAdmin().equals("3")) {
+                Admin backend = new Admin(currentPerson.getUsername(), currentPerson.getFullname(), currentPerson.getMail(), currentPerson.getPhoneNumber(), currentPerson.getAddress());
+                backend(backend);
+            }
+            else if (currentPerson.getAdmin().equals("4")) {
+                Admin order = new Admin(currentPerson.getUsername(), currentPerson.getFullname(), currentPerson.getMail(), currentPerson.getPhoneNumber(), currentPerson.getAddress());
+                order(order);
+            }
+            else if (currentPerson.getAdmin().equals("0")) {
+                Customer KH = new Customer(currentPerson.getUsername(), currentPerson.getFullname(), currentPerson.getMail(), currentPerson.getPhoneNumber(), currentPerson.getAddress());
+                customer(KH);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    static void mainRegister() {
+        Person person = new Person();
+        try {
             person.register();
-            if(connect.checkRegister(cn, person.getUsername(), person.getPassword(), person.getFullname(), person.getMail(), person.getPhoneNumber())) {
-                System.out.println("New page");
-            }
-            else {
-                System.out.println("Press \"ENTER\" to continue, another key to exit");
-                String check = input.nextLine();
-                if (!check.equals("")) break;
-                
-            }
-        } while(true);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
+    
     static void menu() {
         String choice;
         do {
@@ -58,16 +171,17 @@ public class Main {
             choice = input.nextLine();
             
             switch(choice) {
-                case "1": login(); break;
-                case "2": register(); break;
+                case "1": mainLogin(); break;
+                case "2": mainRegister(); break;
                 case "3": System.exit(0);
-                default:
+                default: System.out.println("Please enter again (1-3)");
             }
         } while(true);
     }
 
     public static void main(String[] args) {
         menu();
+
 //        Statement stm = cn.createStatement();
 //        ResultSet rs = null;
         
